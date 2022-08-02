d6036b61da50dd6ddec5a2ca227837ae1efbdd09
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2022-08-02 13:04:07 -0300

    Adicionando funcoes para salvar no localStorage e pegar o dado e estado inicial

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function reducer(state = 0, action) {
+function reducer(state = getStorage(), action) {
     switch (action.type) {
         case "I":
             state = state+=1
@@ -11,14 +11,34 @@ function reducer(state = 0, action) {
     }
 }
 
+function saveStorage(data) {
+    window.localStorage.setItem('counter', data);
+}
+
+function getStorage() {
+    const data = window.localStorage.getItem('counter');
+    if (!!data) return JSON.parse(data);
+    return null;
+}
+
+function getCounter() {
+    return document.querySelector('.counter');
+}
+
 const store = Redux.createStore(reducer);
 
 const buttons = [...document.querySelectorAll('button')];
 buttons.forEach( button => {
-    const count = document.querySelector('.counter');
-    count.textContent = store.getState();
+    const state = store.getState();
+    getCounter().textContent = state;
+    
     button.addEventListener('click', ({ target }) => {
         store.dispatch({ type: target.id});
-        count.textContent = store.getState();
+        getCounter().textContent = store.getState();
+        saveStorage(store.getState());
     })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    getCounter().textContent = getStorage();
 })
